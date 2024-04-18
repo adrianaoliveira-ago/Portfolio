80c1a6e010e69c74ece90eb75f664b9ddc084ad0
Adriana Gonzaga <adrianagonzaga@Adrianas-iMac.fritz.box>
2024-04-18 20:52:58 +0200

    new features

---
+++ src/Header.jsx
@@ -0,0 +1,27 @@
+import "./Header.css";
+
+import GetInTouchButton from "./assets/GetInTouch-Button.svg";
+import waveHeader from "./assets/Wave-header.svg";
+
+const Header = () => {
+  return (
+    <>
+      <div id="page-header">
+        <div className="page-header-ul">
+          <ul className="page-header-li">
+            <li>Work</li>
+            <li>Resume</li>
+            <li>Work</li>
+            {/* <li>Get in touch</li> */}
+          </ul>
+          <img src={GetInTouchButton} className="page-header-button" />
+          <div className="page-header-wave">
+            <img src={waveHeader} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
